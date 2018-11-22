ae2f444dafe4ab4fa60c8d5fd31fca3a543f6e71
Jiyun Park <junepark@theloop.co.kr>
2018-11-23 00:23:58 +0900

    add round 3 in websocket routes

--- dispatcher/game_dispatcher.py
+++ dispatcher/game_dispatcher.py
@@ -8,9 +8,11 @@ from db_manager import db_manager
 
 game_room = dict()
 address_rooms = dict()
-first_tx_result = ''
-player_waiting_condition: Condition = None
-tx_result_condition: Condition = None
+first_start_game_result = ''
+first_reveal_game_result = ''
+condition_player_waiting: Condition = None
+condition_start_game: Condition = None
+condition_reveal_game: Condition = None
 
 
 class GameDispatcher:
@@ -25,11 +27,11 @@ class GameDispatcher:
 
     @staticmethod
     async def game(request, ws):
-        global player_waiting_condition
-        if player_waiting_condition is None:
-            player_waiting_condition = Condition()
+        global condition_player_waiting, game_room, address_rooms
+        if condition_player_waiting is None:
+            condition_player_waiting = Condition()
 
-        # ====== round1 ======
+        # ============================ round1 ============================
         token = await ws.recv()
         if isinstance(token, bytes):
             token = token.decode('utf-8')
@@ -43,49 +45,63 @@ class GameDispatcher:
         if not game_room:
             game_room_id = '0x' + os.urandom(32).hex()
             game_room[game_room_id] = (my_address, None)
-            async with player_waiting_condition:
-                await player_waiting_condition.wait()
+            async with condition_player_waiting:
+                await condition_player_waiting.wait()
         # second player
         else:
             game_room_id, (opposite_address, _) = next(iter(game_room.items()))
             game_room.pop(game_room_id)
 
             GameDispatcher.save_response_to_both_players(game_room_id, my_address, opposite_address)
-            async with player_waiting_condition:
-                player_waiting_condition.notify()
+            async with condition_player_waiting:
+                condition_player_waiting.notify()
 
+        game_room, address_rooms = dict(), dict()  # clear memory
         await ws.send(json.dumps(address_rooms[my_address]))
 
-        # ====== round2 ======
-        global tx_result_condition, first_tx_result
-        if tx_result_condition is None:
-            tx_result_condition = Condition()
+        # ============================ round2 ============================
+        global condition_start_game, first_start_game_result
+        if condition_start_game is None:
+            condition_start_game = Condition()
         start_game_tx_hash = await ws.recv()
 
         # first player
-        if not first_tx_result:
-            while True:
-                try:
-                    response = utils.get_transaction_result(start_game_tx_hash)
-                    first_tx_result = start_game_tx_hash
-                    break
-                except Exception:
-                    time.sleep(0.5)
-            async with tx_result_condition:
-                await tx_result_condition.wait()
+        if not first_start_game_result:
+            result = await GameDispatcher.get_result_loop(start_game_tx_hash)
+            first_start_game_result = start_game_tx_hash
+            async with condition_start_game:
+                await condition_start_game.wait()
         # second player
         else:
-            while True:
-                try:
-                    response = utils.get_transaction_result(start_game_tx_hash)
-                    break
-                except Exception:
-                    time.sleep(0.5)
-            async with tx_result_condition:
-                tx_result_condition.notify()
+            result = await GameDispatcher.get_result_loop(start_game_tx_hash)
+            async with condition_start_game:
+                condition_start_game.notify()
 
-        await ws.send('success')
+        first_start_game_result = ''  # clear memory
+        await ws.send(result)
 
+        # ============================ round3 ============================
+        global first_reveal_game_result, condition_reveal_game
+        if condition_reveal_game is None:
+            condition_reveal_game = Condition()
+        reveal_game_tx_hash = await ws.recv()
+
+        # first player
+        if not first_reveal_game_result:
+            result = await GameDispatcher.get_result_loop(reveal_game_tx_hash)
+            first_reveal_game_result = reveal_game_tx_hash
+            async with condition_reveal_game:
+                await condition_reveal_game.wait()
+        # second player
+        else:
+            result = await GameDispatcher.get_result_loop(reveal_game_tx_hash)
+            async with condition_reveal_game:
+                condition_reveal_game.notify()
+
+        first_reveal_game_result = ''  # clear memory
+        await ws.send(result)
+
+        # TODO end game request!!!
 
     @staticmethod
     def save_response_to_both_players(game_room_id, my_address, opposite_address):
@@ -101,3 +117,13 @@ class GameDispatcher:
             'opposite_address': my_address,
             'opposite_nickname': my_nickname
         }
+
+    @staticmethod
+    async def get_result_loop(start_game_tx_hash):
+        while True:
+            try:
+                response = utils.get_transaction_result(start_game_tx_hash)
+                break
+            except Exception:
+                time.sleep(0.5)
+        return 'success'
